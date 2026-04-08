2c05df5bc1e0a421048da90fec1fc3db5c3f6eee
Moiz-205 <moizrajput205@gmail.com>
2026-04-09 01:12:54 +0500

    updated main file for second testing

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import server
+from python import server, client
+import threading
 
 if __name__ == "__main__":
-    server.start_server("test")
+    threading.Thread(target=server.start_server, args=("test",), daemon=True).start()
+
+    client.connect(host="127.0.0.1", port=5000,
+        username="cat", color="red",
+        room_code="test")
